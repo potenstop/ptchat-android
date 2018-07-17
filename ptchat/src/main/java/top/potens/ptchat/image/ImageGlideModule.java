0acbcd4d5a6a9dc0f04d4f28e875e04fe56b93bc
yanshaowen <yanshaowen@zhiketong.cn>
2018-07-17 10:24:16 +0800

    init

---
+++ ptchat/src/main/java/top/potens/ptchat/image/ImageGlideModule.java
@@ -0,0 +1,10 @@
+package top.potens.ptchat.image;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by wenshao on 2018/4/29.
+ */
+@GlideModule
+public class ImageGlideModule extends AppGlideModule {}
